f4c22d201b5d9dd53f8b616c4aeeb44a932f181b
eroll94 <erolsh94@gmail.com>
2020-10-01 23:27:30 +0200

    finished assignments for table

--- src/test/java/com/tests/TableTest.java
+++ src/test/java/com/tests/TableTest.java
@@ -5,6 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.AfterTest;
 import org.testng.annotations.BeforeTest;
 import org.testng.annotations.Test;
@@ -16,6 +17,7 @@ public class TableTest {
 
 
     private WebDriver driver;
+    String khalifaHeight;
 
     @BeforeTest
     public void setUp() {
@@ -49,7 +51,26 @@ public class TableTest {
         System.out.println("Number of tr is: " + tr.size());
         System.out.println("Number of th is: " + th.size());
 
+        //count how many cells has 6 row
 
+        int col6test = driver.findElements(By.xpath("//*[@id=\"post-body-5867683659713562481\"]/div/div[1]/table/tfoot/tr")).size();
+        String col6test2 = driver.findElement(By.xpath("//*[@id=\"post-body-5867683659713562481\"]/div/div[1]/table/tfoot/tr")).getText();
+        System.out.print("Num of cells is tr: " + col6test + "\n");
+        System.out.print("Text of cells is: : " + col6test2 + "\n");
+
+        if (col6test == 1){
+            System.out.print("It has only one row \n");
+        }
+        else {
+            System.out.print("Nah, it have more than one row \n");
+        }
+
+        // Verify that Burj Khalifa has a height of 829m
+        khalifaHeight = "829m";
+
+        String burj = driver.findElement(By.xpath("//*[@id=\"post-body-5867683659713562481\"]/div/div[1]/table/tbody/tr[1]/td[3]")).getText();
+        System.out.print("Height of burj khalifa is: " + burj);
+        Assert.assertEquals(burj, khalifaHeight,"Height is not match!");
 
 
     }
