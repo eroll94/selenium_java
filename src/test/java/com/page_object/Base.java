509ecae33b050fd422769901528e9c38dab65bfe
eroll94 <erolsh94@gmail.com>
2020-10-12 22:42:40 +0200

    added few new funct to base

--- src/test/java/com/page_object/Base.java
+++ src/test/java/com/page_object/Base.java
@@ -1,8 +1,11 @@
 package com.page_object;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
 
 public class Base {
 
@@ -19,5 +22,30 @@ public class Base {
         return driver;
 
     }
+
+    public WebDriver firefoxDriverConnection(){
+        WebDriverManager.firefoxdriver().setup();
+        driver = new FirefoxDriver();
+        return driver;
+    }
+
+    public void openUrl(String url){
+        driver.get(url);
+
+    }
+
+    public WebElement findElement(By locator){
+        WebElement find = null;
+        try{
+            find = driver.findElement(locator);
+        }
+        catch (Exception e){
+            System.out.print("Exception happened");
+        }
+        return find;
+
+    }
+
+
 
 }
