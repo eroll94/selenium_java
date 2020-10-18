bbdf34431b9372aa6c8d97a7aa6791b5ecaaf467
eroll94 <erolsh94@gmail.com>
2020-10-18 22:44:03 +0200

    finished base class

--- src/test/java/com/page_object/Base.java
+++ src/test/java/com/page_object/Base.java
@@ -2,10 +2,16 @@ package com.page_object;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
 import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.Select;
+import org.openqa.selenium.support.ui.WebDriverWait;
+
+import java.util.List;
 
 public class Base {
 
@@ -44,6 +50,82 @@ public class Base {
         }
         return find;
 
+    }
+
+    public List<WebElement> findElements(By locator){
+        return driver.findElements(locator);
+    }
+
+    public String getPageTitle(){
+        return driver.getTitle();
+    }
+
+    public String getText(By locator) {
+        return driver.findElement(locator).getText();
+    }
+
+    public void clickElement(By locator) {
+        driver.findElement(locator).click();
+    }
+
+    public void clickElement(WebElement element){ element.click(); }
+
+    public String getAttribute(By locator, String attribute){
+        WebElement element = findElement(locator);
+        return element.getAttribute(attribute);
+    }
+
+    public boolean isClickable(By locator, int timeOutSeconds){
+        boolean result;
+        try{
+            WebDriverWait waitForLocator = new WebDriverWait(driver, timeOutSeconds);
+            waitForLocator.until(ExpectedConditions.elementToBeClickable(locator));
+            result = driver.findElement(locator).isDisplayed();
+        }
+        catch (org.openqa.selenium.NoSuchElementException e) {
+            result = false;
+        }
+        return result;
+    }
+
+    public void clickWhenReady(By locator, int timeOutSeconds){
+        try{
+            WebDriverWait waitForLocator = new WebDriverWait(driver, timeOutSeconds);
+            waitForLocator.until(ExpectedConditions.elementToBeClickable(locator));
+            if(driver.findElement(locator).isDisplayed()){
+                this.clickElement(locator);
+            }
+        }
+        catch (org.openqa.selenium.NoSuchElementException e) {
+            System.out.println("Exception waiting for a locator to be clicked.");
+        }
+    }
+
+    public void isDisplayed(By locator){
+        try {
+            WebDriverWait waitForLocator = new WebDriverWait(driver, 10);
+            waitForLocator.until(ExpectedConditions.visibilityOfElementLocated(locator));
+        }
+        catch (org.openqa.selenium.NoSuchElementException e) {
+            System.out.println("Exception waiting for a locator to be visible.");
+        }
+    }
+
+    public void refreshPage(By locator) {
+        driver.navigate().refresh();
+        isClickable(locator, 10);
+    }
+
+    public void sendKeys(By locator, String keysToSend){
+        driver.findElement(locator).sendKeys(keysToSend);
+    }
+
+    public void sendKeys(By locator, Keys keyToSend){
+        driver.findElement(locator).sendKeys(Keys.chord(Keys.CONTROL, "a"), keyToSend);
+    }
+
+    public Select getDropDownElement(String dropDownName){
+        return new Select(driver.findElement(By.name(dropDownName)));
     }
 
 
